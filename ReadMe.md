b3ce6e40f38c0713d3b12d00007c29a32c9f0786
Seeta <srvadali@gmail.com>
2017-12-31 08:48:05 +0100

    Added custom Eq type class instance

---
+++ ReadMe.md
@@ -0,0 +1,12 @@
+# Scala With Cats
+
+I am writing notes to myself when I am reading book called [scala with cats](https://underscore.io/books/scala-with-cats/). 
+cat word is too much over loaded so I have changed model data from `Cat` to `Person` 
+ 
+
+# Chapter 1 (Introduction)
+
+ - Type classes
+ - Cats Show
+ - Cats Eq
+ - Variance
---
+++ src/main/scala/com/seeta/chapter1/typeclass/CatsEqDemo.scala
@@ -0,0 +1,55 @@
+package com.seeta.chapter1.typeclass
+
+import java.util.Date
+
+import cats.instances.int._
+import cats.instances.string._
+import cats.instances.long._
+import cats.instances.option._
+import cats.Eq
+import cats.syntax.eq._
+import cats.syntax.option._
+import com.seeta.Person
+
+/**
+ * ==Cats Eq==
+ * is another type class which supports ''type-safe equality''
+ *
+ * == Custom Eq Type==
+ * custom type can be defined with ''Eq.instance'' method which accepts
+ * a function of type ''(A, A) => Boolean''
+ *
+ *
+ * @author Seeta (Ramayya) Vadali
+ */
+
+object EqCustomInstances {
+  implicit val eqDate: Eq[Date] = Eq.instance[Date] { (date1, date2) =>
+    date1.getTime === date2.getTime
+  }
+
+  implicit val eqPerson: Eq[Person] = Eq.instance[Person] { (p1, p2) =>
+    (p1.name === p2.name) && (p1.age === p2.age) && (p1.color === p2.color)
+  }
+}
+
+object CatsEqDemo extends App {
+
+  println(123 === 123)
+  println(Option(123) === Option.empty[Int])
+
+  println(123.some === none[Int])
+
+  // Different types: Option[String] & Option[Int] comparision
+  // expected compile time error with cats Eq that is possible
+  println("123".some == none[Int])
+
+  import EqCustomInstances._
+  val date1 = new Date()
+  val date2 = new Date()
+
+  println(date1 === date2)
+
+  println(Person("Seeta Vadali", 36, "brown").some === none[Person])
+  println(Person("Seeta Vadali", 36, "brown").some === Person("Seeta Vadali", 36, "brown").some)
+}
